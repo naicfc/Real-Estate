e08012d464e710a58f4aa31c6f086e8c73a0691a
Godfred <godfrednai@gmail.com>
2023-06-22 18:52:18 +0000

    added recentcard component

--- src/components/recent/Recent.jsx
+++ src/components/recent/Recent.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from "react";
+import RecentCard from "./RecentCard";
+import Image1 from '../../assets/images/project1.jpg'
+import Image2 from "../../assets/images/project4.jpg";
+import Image3 from "../../assets/images/project6.jpg";
 
 export default function Recent() {
   return (
@@ -8,8 +12,36 @@ export default function Recent() {
           Recently Listed Property
         </p>
         <p className="text-sm text-gray-600 font-medium w-full md:w-[70%] mx-auto">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia at asperiores praesentium mollitia ipsam similique consectetur veniam doloribus voluptas commodi.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia at
+          asperiores praesentium mollitia ipsam similique consectetur veniam
+          doloribus voluptas commodi.
         </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+        <RecentCard
+          url={Image1}
+          status={"available"}
+          name={"Nai properties"}
+          location={"La Palmwine"}
+          price={"$62,000"}
+          type={"apartment"}
+        />
+        <RecentCard
+          url={Image2}
+          status={"available"}
+          name={"Nai properties"}
+          location={"La Palmwine"}
+          price={"$20,000"}
+          type={"apartment"}
+        />
+        <RecentCard
+          url={Image3}
+          status={"available"}
+          name={"Nai properties"}
+          location={"La Palmwine"}
+          price={"$39,000"}
+          type={"apartment"}
+        />
       </div>
     </div>
   );
