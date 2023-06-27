80cdaea45e5f48d88d9060f88b19ae8f899066fa
Godfred <godfrednai@gmail.com>
2023-06-27 08:44:27 +0000

    added explore card component

---
+++ src/components/explore/Explore.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Explore() {
+  return (
+    <div className=" px-4 lg:px-28 py-16">
+      <div className="text-center mb-10">
+        <p className="text-xl mb-2 tracking-wider text-gray-800 font-semibold capitalize">
+          explore by location
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+      </div>
+    </div>
+  );
+}
