aff8fef934ce2155da3b56880adc78c474205565
Godfred <godfrednai@gmail.com>
2023-07-13 14:35:07 +0000

    completed pricing page

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Banner from '../components/banner/Banner';
+import Pricings from '../components/packages/Pricing';
+import Questions from '../components/questions/Questions';
+import LinkFooter from '../components/footer/LinkFooter';
+import Footer from '../components/footer/Footer';
 
 export default function Pricing() {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center">
-      <div className="text-gray-600">Pricing</div>
-    </div>
+    <>
+      <Banner title="30 days money back guarantee." description="No Extra Fees. Friendly Support"/>
+      <Pricings />
+      <Questions />
+      <LinkFooter />
+      <Footer />
+    </>
   );
 }
