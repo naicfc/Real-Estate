c62b494fdd38af9406dae01d91b228a3c9126136
Godfred <godfrednai@gmail.com>
2023-07-18 19:18:16 +0000

    added contact component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Banner from '../components/banner/Banner';
-import Agents from '../components/agents/Agents';
-import Questions from '../components/questions/Questions';
-import LinkFooter from '../components/footer/LinkFooter';
-import Footer from '../components/footer/Footer';
+import React from "react";
+import Banner from "../components/banner/Banner";
+import Questions from "../components/questions/Questions";
+import LinkFooter from "../components/footer/LinkFooter";
+import Footer from "../components/footer/Footer";
+import Contacts from "../components/contact/Contact";
 
 export default function Contact() {
   return (
     <>
-     <Banner title="contact us" description="Get Help & Friendly Support"/>
-     <Agents />
-     <Questions />
-     <LinkFooter />
-     <Footer />
+      <Banner title="contact us" description="Get Help & Friendly Support" />
+      <Contacts />
+      <Questions />
+      <LinkFooter />
+      <Footer />
     </>
   );
 }
