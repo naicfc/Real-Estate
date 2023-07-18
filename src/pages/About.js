3aa2a338c68ff64217f8c2cd155948738ca2066b
Godfred <godfrednai@gmail.com>
2023-07-18 19:17:34 +0000

    minor UI changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Banner from '../components/banner/Banner';
+import Questions from '../components/questions/Questions';
+import LinkFooter from '../components/footer/LinkFooter';
+import Footer from '../components/footer/Footer';
 
 export default function About() {
   return (
     <>
       <Banner title="about" description="About Us - Who We Are?"/>
+      <Questions />
+      <LinkFooter />
+      <Footer />
     </>
   );
 }
